741656664e7959670b1d2ad9ea50344e0036d42f
JackJeysonAR <145811609+JackJeysonAR@users.noreply.github.com>
2025-05-22 12:17:57 -0500

    Delete dev.ts

--- dev.ts
+++
@@ -1,6 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/suggest-accessible-routes.ts';
-import '@/ai/flows/summarize-driver-accessibility-reviews.ts';
-import '@/ai/flows/get-location-accessibility-insights.ts';
